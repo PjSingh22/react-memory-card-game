fc11e93c897c10cb71dad2de9b88e6b16a910af2
Prabhjot Singh <pjsingh1359@gmail.com>
2022-07-19 16:36:12 -0700

    add puppy photos

---
+++ src/components/Card.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default function Card({ image, name }) {
+  const [checked, setChecked] = React.useState(false);
+  const [cardImg, setCardImg] = React.useState(image);
+  const [cardName, setCardName] = React.useState(name);
+
+  React.useEffect(() => {
+    setCardImg(image);
+    setCardName(name);
+  }
+  , [image, name]);
+
+  return (
+    <div className="card">
+      <div className="card-image">
+        <img src={cardImg} alt={cardName} />
+      </div>
+      <div className="card-name">
+        {cardName}
+      </div>
+    </div>
+  )
+}
