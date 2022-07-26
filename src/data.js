c42dc3532fe0c6f6b7e37f6aba96390511d72cc1
Prabhjot Singh <pjsingh1359@gmail.com>
2022-07-26 12:27:26 -0700

    add data

---
+++ src/data.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+export const data = [
+  {
+    name: 'Australian Shepherd',
+    url: './images/aussy.jpg'
+  },
+  {
+    name: 'Chow Chow',
+    url: './images/chow-chow.jpg'
+  },
+  {
+    name: 'Corgi',
+    url: './images/corgi.jpg'
+  },
+  {
+    name: 'Dachshund',
+    url: './images/dachshund.jpg'
+  },
+  {
+    name: 'German Shepherd',
+    url: './images/german.jpg'
+  },
+  {
+    name: 'Golden Retriever',
+    url: './images/goldy.jpg'
+  },
+  {
+    name: 'Pomeranian',
+    url: './images/pomeranian.jpg'
+  },
+  {
+    name: 'Poodle',
+    url: './images/poodle.jpg'
+  },
+  {
+    name: 'Shiba Inu',
+    url: './images/shiba.jpg'
+  },
+  {
+    name: 'Yorkshire Terrier',
+    url: './images/yorki.jpg'
+  }
+];
