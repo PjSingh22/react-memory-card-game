d551adf5e5948645b0084095973768b0367a3650
Prabhjot Singh <pjsingh1359@gmail.com>
2022-07-26 16:06:15 -0700

    add score card html

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Card from './components/Card';
@@ -6,13 +7,19 @@ import goldy from './images/goldy.jpg';
 
 
 function App() {
+  const [currentScore, setCurrentScore] = useState(0);
+  const [topScore, setTopScore] = useState(0);
 
   return (
     <div className="App">
       <header className="app-header">Puppy Memory Game</header>
+      <div className='score-container'>
+        <div className='current-score'>Current Score: <span className='current-score-num'>{ currentScore }</span></div>
+        <div className='high-score'>High Score: <span className='high-score-num'>{ topScore }</span></div>
+      </div>
       <div className="card-container">
         {data.map(dog => (
-          <Card key={dog.name} name={dog.name} image={dog.url} />
+          <Card key={dog.name} name={dog.name} image={dog.url} setCurrentScore={setCurrentScore} currentScore={currentScore} topScore={topScore} setTopScore={setTopScore} />
         ))}
       </div>
     </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 
-export default function Card({ image, name }) {
+export default function Card({ image, name, setCurrentScore, setTopScore, currentScore, topScore }) {
   const [checked, setChecked] = React.useState(false);
   const [cardImg, setCardImg] = React.useState(image);
   const [cardName, setCardName] = React.useState(name);
+
+  const increaseScore = () => {
+    setCurrentScore(currentScore => currentScore + 1);
+    if (checked) {
+      setChecked(false);
+      return gameOver();
+    }
+    setChecked(true);
+  }
+
+  const gameOver = () => {
+    setTopScore(currentScore);
+    setCurrentScore(0);
+  }
 
   React.useEffect(() => {
     setCardImg(image);
@@ -12,7 +26,7 @@ export default function Card({ image, name }) {
   , [image, name]);
 
   return (
-    <div className="card">
+    <div className="card" onClick={increaseScore}>
       <div className="card-image" style={{ backgroundImage: `url(${cardImg})` }}>
         {/* <img src={cardImg} alt={cardName} /> */}
       </div>
