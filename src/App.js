f55142eff576b6773d52ca652b9869ae44eaded2
Prabhjot Singh <pjsingh1359@gmail.com>
2022-07-26 14:10:26 -0700

    add color and font to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import Card from './components/Card';
-
+import {data} from './data';
 import goldy from './images/goldy.jpg';
 
+
 function App() {
+
   return (
     <div className="App">
+      <header className="app-header">Puppy Memory Game</header>
+      <div className="card-container">
+
+      </div>
       <Card image={goldy} name="golden retriever" />
     </div>
   );
